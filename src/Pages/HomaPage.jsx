dde9627af22053faf089cc125f6493f8a645a260
Kenzywaheed <kenzywaheed6@gmailcom>
2026-01-19 18:20:38 +0200

    Fix import path case

--- src/Pages/HomaPage.jsx
+++ src/Pages/HomaPage.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Slider from "../components/Slider/Slider";
 // import OurSolutions from "../components/OurSolutions/OurSolutions";
-import Testimonials from "../components/Testimonials/Testimonials";
-import OurClients from "../components/ourClients/ourClients";
-import OurParents from "../components/ourParents/OurParents";
-import Overview from "../components/Overview/Overview";
+// import Testimonials from "../components/Testimonials/Testimonials";
+// import OurClients from "../components/OurClients/OurClients";
+// import OurParents from "../components/ourParents/OurParents";
+// import Overview from "../components/Overview/Overview";
 import GetInTouch from "../components/getInTouch/getInTouch";
-import Ourlocations from "../components/Ourlocations/Ourlocations";
+// import Ourlocations from "../components/Ourlocations/Ourlocations";
 import OurServices from "../components/OurServices/OurServices";
-import FAQ from "../components/FAQ/FAQ";
-import FeedBack from "../components/FeedBack/FeedBack";
+// import FAQ from "../components/FAQ/FAQ";
+// import FeedBack from "../components/FeedBack/FeedBack";
 import AboutPage from "./About/AboutPage";
 // import QuestionOne from "../components/Questions/QuestionOne";
 
